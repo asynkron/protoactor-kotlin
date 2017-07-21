ec8368785ffc4fd128ae84b7c75a2680b6d2187f
rogeralsing <rogeralsing@gmail.com>
2017-07-21 15:12:39 +0200

    fix slow test

--- proto.actor/src/test/kotlin/actor/proto/tests/ReceiveTimeoutTests.kt
+++ proto.actor/src/test/kotlin/actor/proto/tests/ReceiveTimeoutTests.kt
@@ -1,10 +1,11 @@
 package actor.proto.tests
 
 import actor.proto.*
-import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import java.time.Duration
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 class ReceiveTimeoutTests {
     @Test fun `receive timeout received within expected time`() {
@@ -28,7 +29,7 @@ class ReceiveTimeoutTests {
     }
 
     @Test fun `receive timeout not received within expected_time`() {
-        var timeoutReceived: Boolean = false
+        val cd = CountDownLatch(1)
         val props: Props = fromFunc {
             val tmp = message
             when (tmp) {
@@ -36,19 +37,18 @@ class ReceiveTimeoutTests {
                     setReceiveTimeout(Duration.ofMillis(1500))
                 }
                 is ReceiveTimeout -> {
-                    timeoutReceived = true
+                    cd.countDown()
                 }
             }
 
         }
 
         spawn(props)
-        Thread.sleep(1500)
-        assertFalse(timeoutReceived)
+        cd.await(1000,TimeUnit.MILLISECONDS)
     }
 
     @Test fun `can cancel receive timeout`() {
-        var timeoutReceived: Boolean = false
+        val cd = CountDownLatch(1)
         val props: Props = fromFunc {
             val tmp = message
             when (tmp) {
@@ -57,19 +57,18 @@ class ReceiveTimeoutTests {
                     cancelReceiveTimeout()
                 }
                 is ReceiveTimeout -> {
-                    timeoutReceived = true
+                    cd.countDown()
                 }
             }
 
         }
 
         spawn(props)
-        Thread.sleep(1500)
-        assertFalse(timeoutReceived)
+       cd.await(1000,TimeUnit.MILLISECONDS)
     }
 
     @Test fun `can still set receive timeout after cancelling`() {
-        var timeoutReceived: Boolean = false
+        val cd = CountDownLatch(1)
         val props: Props = fromFunc {
             val tmp = message
             when (tmp) {
@@ -79,15 +78,14 @@ class ReceiveTimeoutTests {
                     setReceiveTimeout(Duration.ofMillis(150))
                 }
                 is ReceiveTimeout -> {
-                    timeoutReceived = true
+                    cd.countDown()
                 }
             }
 
         }
 
         spawn(props)
-        Thread.sleep(1500)
-        assertTrue(timeoutReceived)
+        cd.await(1000,TimeUnit.MILLISECONDS)
     }
 }
 
