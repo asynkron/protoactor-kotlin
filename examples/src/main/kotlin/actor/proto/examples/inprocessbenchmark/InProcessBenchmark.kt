88cc417ffbc7b07ac1b016c1aab89287e0b875ec
rogeralsing <rogeralsing@gmail.com>
2017-07-21 16:39:10 +0200

    .

--- examples/src/main/kotlin/actor/proto/examples/inprocessbenchmark/InProcessBenchmark.kt
+++ examples/src/main/kotlin/actor/proto/examples/inprocessbenchmark/InProcessBenchmark.kt
@@ -19,7 +19,7 @@ fun run() {
     val messageCount = 1_000_000
     val batchSize = 500
     println("Dispatcher\t\tElapsed\t\tMsg/sec")
-    val tps = arrayOf(300, 400, 500, 600, 700, 800, 900)
+    val tps = arrayOf(1,2,5,10,50,100,200,300, 400, 500, 600, 700, 800, 900)
     for (t in tps) {
         val d = ThreadPoolDispatcher(throughput = t)
         val clientCount = Runtime.getRuntime().availableProcessors() * 2
