d7faea8453d359fd7da49e1d26e42e0e84a4b62e
rogeralsing <rogeralsing@gmail.com>
2017-07-28 10:35:31 +0200

    pid

--- examples/src/main/kotlin/actor/proto/examples/helloworld_java/hello.java
+++ examples/src/main/kotlin/actor/proto/examples/helloworld_java/hello.java
@@ -1,16 +1,15 @@
 package actor.proto.examples.helloworld_java;
 
 import actor.proto.Props;
-import actor.proto.Protos;
-
 import java.io.IOException;
 
 import static actor.proto.Actors.*;
+import static actor.proto.Protos.*;
 
 public class hello {
     public static void main(String[] args) throws IOException {
         Props p = fromFutureProducer(HelloActor::new);
-        Protos.PID pid = spawn(p);
+        PID pid = spawn(p);
         send(pid,"Proto.Actor Java");
 
         System.in.read();
