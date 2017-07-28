c161e931661be9b97eebd67c7cba393f9d41cc67
rogeralsing <rogeralsing@gmail.com>
2017-07-28 10:16:13 +0200

    .

--- examples/src/main/kotlin/actor/proto/examples/helloworld_java/HelloActor.java
+++ examples/src/main/kotlin/actor/proto/examples/helloworld_java/HelloActor.java
@@ -5,9 +5,6 @@ import actor.proto.FutureContext;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import static actor.proto.Actors.done;
 
