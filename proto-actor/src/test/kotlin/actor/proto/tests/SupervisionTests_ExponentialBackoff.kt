39452e274d86d5ce677daca1d2dee56d586d4ccc
Örjan Sjöholm <orjansjoholm@gmail.com>
2017-08-06 08:00:38 +0200

    Activated SupervisionTests_ExponentialBackoff tests

--- proto-actor/src/test/kotlin/actor/proto/tests/SupervisionTests_ExponentialBackoff.kt
+++ proto-actor/src/test/kotlin/actor/proto/tests/SupervisionTests_ExponentialBackoff.kt
@@ -1,22 +1,46 @@
-//package proto.tests
-//
-//import actor.proto.ExponentialBackoffStrategy
-//import actor.proto.RestartStatistics
-//import org.junit.Assert
-//import java.time.Duration
-//
-//class SupervisionTests_ExponentialBackoff {
-//    fun failureOutsideWindow_ZeroCount () {
-//        val rs : RestartStatistics = RestartStatistics(10, DateTime.now.subtract(TimeSpan.fromSeconds(11)))
-//        val strategy : ExponentialBackoffStrategy = ExponentialBackoffStrategy(Duration.ofSeconds(10), Duration.ofSeconds(1))
-//        strategy.handleFailure(null, null, rs, null)
-//        Assert.assertEquals(0, rs.failureCount)
-//    }
-//    fun failureInsideWindow_IncrementCount () {
-//        val rs : RestartStatistics = RestartStatistics(10, DateTime.now.subtract(TimeSpan.fromSeconds(9)))
-//        val strategy : ExponentialBackoffStrategy = ExponentialBackoffStrategy(TimeSpan.fromSeconds(10), TimeSpan.fromSeconds(1))
-//        strategy.handleFailure(null, null, rs, null)
-//        Assert.assertEquals(11, rs.failureCount)
-//    }
-//}
-//
+package actor.proto.tests
+
+import actor.proto.*
+import org.junit.Assert
+import org.junit.Test
+import java.time.Duration
+
+class SupervisionTests_ExponentialBackoff {
+    @Test fun `a failure outside window should zero the count` () {
+        val lastFailureIsOlderThanWindow = java.lang.System.currentTimeMillis() - java.time.Duration.ofSeconds(11).toMillis()
+        val rs : RestartStatistics = RestartStatistics(10, lastFailureIsOlderThanWindow)
+        val strategy : ExponentialBackoffStrategy = ExponentialBackoffStrategy(Duration.ofSeconds(10), Duration.ofSeconds(1))
+
+        strategy.handleFailure(DummySupervisor(), dummyPID(), rs, Exception())
+
+        Assert.assertEquals(0, rs.failureCount)
+    }
+
+
+    @Test fun `a failure inside window should increment count` () {
+        val lastFailureIsNewerThanWindow = java.lang.System.currentTimeMillis() - java.time.Duration.ofSeconds(9).toMillis()
+        val rs : RestartStatistics = RestartStatistics(10, lastFailureIsNewerThanWindow)
+        val strategy : ExponentialBackoffStrategy = ExponentialBackoffStrategy(Duration.ofSeconds(10), Duration.ofSeconds(1))
+
+        strategy.handleFailure(DummySupervisor(), dummyPID(), rs, Exception())
+
+        Assert.assertEquals(11, rs.failureCount)
+    }
+}
+
+class DummySupervisor : Supervisor {
+    override val children: Collection<PID>
+        get() = emptyList()
+
+    override fun escalateFailure(reason: Exception, who: PID) { }
+
+    override fun restartChildren(reason: Exception, vararg pids: PID) { }
+
+    override fun stopChildren(vararg pids: PID) { }
+
+    override fun resumeChildren(vararg pids: PID) { }
+}
+
+fun dummyPID(): PID {
+    return PID("", "")
+}
