5a1eb48ed2a84f057805550a694296085beb4b02
James Cobb <james@crowdconnected.co.uk>
2018-02-04 13:00:26 +0000

    Revert "Add slf4j logging"
    This reverts commit 3af326a555715fde99f4aaacb8803256a3699726.

--- proto-remote/src/main/kotlin/actor/proto/remote/EndpointWriter.kt
+++ proto-remote/src/main/kotlin/actor/proto/remote/EndpointWriter.kt
@@ -5,14 +5,9 @@ import com.google.protobuf.ByteString
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import io.grpc.stub.StreamObserver
-import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
 
-class EndpointWriter(private val address: String, private val config: RemoteConfig) : Actor {
-    companion object {
-        private val LOGGER = LoggerFactory.getLogger(EndpointWriter::class.java)
-    }
-
+class EndpointWriter(private val address : String, private val config : RemoteConfig) : Actor {
     private var serializerId: Int = 0
     private lateinit var channel: ManagedChannel
     private lateinit var client: RemotingGrpc.RemotingStub
@@ -65,7 +60,7 @@ class EndpointWriter(private val address: String, private val config: RemoteConf
             streamWriter.onNext(batch)
         } catch (x: Exception) {
             stash()
-            LOGGER.error("gRPC Failed to send to address $address, reason ${x.message}")
+            println("gRPC Failed to send to address $address, reason ${x.message}")
             throw  x
         }
     }
@@ -73,7 +68,7 @@ class EndpointWriter(private val address: String, private val config: RemoteConf
     private suspend fun restarting() = channel.shutdownNow()
     private suspend fun stopped() = channel.shutdownNow()
     private suspend fun started() {
-        LOGGER.error("Connecting to address $address")
+        println("Connecting to address $address")
         val (host, port) = parseAddress(address)
         var channelBuilder = ManagedChannelBuilder
                 .forAddress(host, port)
@@ -91,20 +86,19 @@ class EndpointWriter(private val address: String, private val config: RemoteConf
             override fun onNext(value: RemoteProtos.Unit?) {
                 //never called
             }
-
             override fun onCompleted() {
                 //never called
             }
-
             override fun onError(t: Throwable?) {
                 //According to gRPC docs any call to error is the final call and signals termination
                 //val status = Status.fromThrowable(t)
                 val terminated: EndpointTerminatedEvent = EndpointTerminatedEvent(address)
                 EventStream.publish(terminated)
-                LOGGER.error("Lost connection to address $address")
+                println("Lost connection to address $address")
             }
         })
-        LOGGER.error("Connected to address $address")
+
+        println("Connected to address $address")
     }
 }
 
