ae20cf301977349e26224c2a8844b0ccbe303837
James Cobb <james@crowdconnected.co.uk>
2019-08-18 17:54:48 +0100

    Reimplement original Dispatcher that accepted a custom CoroutineDispatcher in the constructor

---
+++ proto-mailbox/src/test/kotlin/actor/proto/mailbox/DefaultDispatcherTest.kt
@@ -0,0 +1,74 @@
+package actor.proto.mailbox
+
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import org.awaitility.Awaitility
+import org.junit.Assert
+import org.junit.Test
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.ContinuationInterceptor
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
+
+class DefaultDispatcherTest {
+
+    @Test
+    fun coroutineScopeTest() {
+        val mailbox1 = TestMailbox(100, true)
+        val mailbox2 = TestMailbox(300, false)
+        val mailbox3 = TestMailbox(100, false)
+
+        val dispatcher1 = DefaultDispatcher()
+        val threadPool = Executors.newWorkStealingPool().asCoroutineDispatcher()
+        val dispatcher2 = DefaultDispatcher(threadPool, 300)
+
+        dispatcher1.schedule(mailbox1)
+        dispatcher1.schedule(mailbox2)
+        dispatcher2.schedule(mailbox3)
+
+
+        Awaitility.ignoreExceptionsByDefault()
+        Awaitility.await().atMost(5, TimeUnit.SECONDS).untilAsserted {
+            Assert.assertTrue(mailbox1.completedRunning)
+            //if the exception in mailbox1 causes all child coroutines to cancel, then this will fail
+            Assert.assertTrue(mailbox2.completedRunning)
+            //mailboxes using same dispatcher should have same coroutine dispatcher
+            Assert.assertSame(mailbox1.runInContext?.get(ContinuationInterceptor.Key), mailbox2.runInContext?.get(ContinuationInterceptor.Key))
+            //but mailboxes with a different dispatcher have a different coroutine dispatcher
+            Assert.assertNotSame(mailbox1.runInContext?.get(ContinuationInterceptor.Key), mailbox3.runInContext?.get(ContinuationInterceptor.Key))
+        }
+    }
+}
+
+
+class TestMailbox(private val millis: Long, private val exception: Boolean) : Mailbox {
+
+    var runInContext: CoroutineContext? = null
+    var completedRunning : Boolean = false
+
+    override fun postSystemMessage(msg: Any) {
+
+    }
+
+    override fun registerHandlers(invoker: MessageInvoker, dispatcher: Dispatcher) {
+
+    }
+
+    override fun start() {
+
+    }
+
+    override suspend fun run() {
+        runInContext = coroutineContext
+        delay(millis)
+        completedRunning = true
+        if (exception) throw Exception()
+    }
+
+    override fun postUserMessage(msg: Any) {
+
+    }
+
+}
